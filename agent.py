2ce527e190e5a02aa04049352e32e29dbc925dc9
Idris Khenchil <idriskhenchil@gmail.com>
2025-02-18 18:10:01 -0800

    IC

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ from mistralai import Mistral
 import discord
 
 MISTRAL_MODEL = "mistral-large-latest"
-SYSTEM_PROMPT = "You are a helpful assistant."
+SYSTEM_PROMPT = "You are not a helpful assistant."
 
 
 class MistralAgent:
